f90a8c81a41c882e5e331e60817736695f7f49c8
Natalyyy18 <153389652+Natalyyy18@users.noreply.github.com>
2024-03-03 16:07:24 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary10;
+
+namespace Laba10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //BankCard card = new BankCard();
+            //card.RandomInit();
+            //card.Show();
+        }
+    }
+}
